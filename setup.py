d98d57ee38a7074102f06b943871c09010e4b634
Narongdej Sarnsuwan <zenyaith@gmail.com>
2016-05-07 16:59:22 +0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='pythaiwordcut',
+    version='0.0.1',
+    description='Simple Thai Wordcut in Python using Maximum Matching',
+    long_description=readme,
+    author='Narongdej Sarnsuwan',
+    author_email='narongdej@sarnsuwan.com',
+    url='https://github.com/zenyai/pythaiwordcut',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
