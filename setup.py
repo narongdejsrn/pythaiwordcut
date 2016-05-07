d4e364bbe4717511e59688e23bdd620f2541e001
Narongdej Sarnsuwan <zenyaith@gmail.com>
2016-05-08 02:22:15 +0700

    Remove pip deployment temporary

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+import os, sys
 
 with open('README') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
 
 setup(
     name='pythaiwordcut',
-    version='0.0.6',
+    version='0.0.9',
     description='Simple Thai Wordcut in Python using Maximum Matching',
     long_description=readme,
     author='Narongdej Sarnsuwan',
     author_email='narongdej@sarnsuwan.com',
     url='https://github.com/zenyai/pythaiwordcut',
-    license=license,
+    license="MIT",
     packages=find_packages(exclude=('tests', 'docs')),
     package_data={
-      'pythaiwordcut.data': ['*.db'],
+      '': ['*.db']
    },
+   zip_safe=False,
    include_package_data=True
 )
