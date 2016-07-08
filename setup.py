cb5bde48f676196e14d547837a072dab3a5775fa
Narongdej Sarnsuwan <zenyaith@gmail.com>
2016-07-08 16:46:47 +0700

    remove c

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 
 from setuptools import setup, find_packages
 import os, sys
-from Cython.Build import cythonize
 
 with open('README.md') as f:
     readme = f.read()
@@ -12,7 +11,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='pythaiwordcut',
-    version='0.1.13',
+    version='0.1.14',
     description='Simple Thai Wordcut in Python using Maximum Matching',
     long_description=readme,
     author='Narongdej Sarnsuwan',
